4b8b39b49c351567eff11272044b7aa7314dae3d
tjoeun42 <tjoeunit@gmail.com>
2025-11-07 16:57:47 +0900

    1일차_2교시_02.style

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  // javaScript 영역
+  // 한줄 주석
+  /*
+    여러줄 주석
+  */
+  return (  // 주석
+    <> {/* 여러줄 일 때는 반드시 최상위 태그가 있어야 한다 */}
+      <h1>REACT TEST하고 있음</h1>
+      <p>오늘부터 react 시작입니다</p>
+      {/* 주석 */}
+    </>
+  );
+}
+
+export default App;
---
+++ 02.style/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // 별도의 style파일을 만들어서 사용
+  return (
+    <>
+      <h1>더조은에 오신것을 환영합니다</h1>
+      <h3>java 풀스택 웹 앱 개발자 과정</h3>
+      <p>class로 react style적용하기</p>
+      <p>id로 스타일 적용하기</p>
+    </>
+  );
+}
+
+export default App;
