6080419faa7beeb77bd430b745f06d06be85e414
tjoeun42 <tjoeunit@gmail.com>
2025-11-11 21:38:06 +0900

    3일차_7교시_13.todolist

--- 13.todolist/src/App.js
+++ 13.todolist/src/App.js
@@ -48,7 +48,7 @@ function App() {
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List />
+      <List todos={todos}/>
     </div>
   );
 }
--- 13.todolist/src/component/TodoItem.js
+++ 13.todolist/src/component/TodoItem.js
@@ -1,5 +1,11 @@
 import "./TodoItem.css";
-const TodoItem = () => {
+// 1. 로 넘어오고 props로 받으면
+// const TodoItem = (props) => {  사용시 : props.todos.id   props.todos.isDone 
+// 1. 로 넘어오고 객체분해할당으로 받으면  
+// const TodoItem = ({todos}) => { 사용시 : todos.id   todos.isDone 
+
+// 2. 로 넘어오고 객체분해할당으로 받으면
+const TodoItem = ({id, isDone, content, date}) => {
     return (
         <div className="todoItem">
             <input type="checkbox" />
