f9806e8e0ae31de88971d2b077c3138bc2deba5c
tjoeun42 <tjoeunit@gmail.com>
2025-11-13 16:15:19 +0900

    5일차_2교시_axios

--- 14.homepage/src/App.js
+++ 14.homepage/src/App.js
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './App.css';
-import {Container, Nav, Navbar, Row, Col} from 'react-bootstrap';
+import {Container, Nav, Navbar, Row, Col, Button} from 'react-bootstrap';
 import pList from './data/ProductList';
 import { Route, Routes, Link, Navigate, useNavigate } from 'react-router-dom';
 import Detail from './pages/Detail';
+import axios from 'axios';
 
 // ajax 사용하기
 // 설치하기 : npm install axios
 function App() {
   const [clothes, setClothes] = useState(pList);
+  const [clickCount, setClickCount] = useState(2);
   let navigate = useNavigate();
 
   return (
@@ -39,6 +41,22 @@ function App() {
                 }
               </Row>
             </Container>
+            <Button variant="success" onClick={() => {
+              axios.get(`https://raw.githubusercontent.com/bliss42/myHome/refs/heads/main/react/data/data${clickCount}.json`)
+                   .then(result => {
+                      // 성공 핸들링
+                      console.log(result);
+                      console.log(result.data);
+                      let copy = [...clothes, ...result.data]
+                      setClothes(copy)
+                      setClickCount(clickCount+1);
+                    })
+                    .catch(() => {
+                      // 에러 핸들링
+                      console.log("axios 실패");
+                      alert('더이상 상품이 없습니다');
+                    });
+            }}>서버에서 데이터 가져오기</Button>
           </>
         } />
         <Route path="/detail/:pid" element={<Detail clothes={clothes}/>} />
@@ -52,7 +70,7 @@ function App() {
 function PListCol(props) {
   return (
     <>
-      <Col>
+      <Col md={4}>
         <img src={`${process.env.PUBLIC_URL}/img/clothes${props.clothes.id}.png`} width="80%"/>
         <h4>{props.clothes.title}</h4>
         <p>{props.clothes.price}</p>
