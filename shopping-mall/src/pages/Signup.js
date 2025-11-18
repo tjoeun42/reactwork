b89315b2d8b3b441eba5bfa487979494a52a7e82
tjoeun42 <tjoeunit@gmail.com>
2025-11-18 21:00:23 +0900

    springboot연ㄴ동

--- shopping-mall/src/pages/Signup.js
+++ shopping-mall/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
-import {Col, Form, Row, Button} from 'react-bootstrap';
+import {Col, Form, Row, Button, Modal} from 'react-bootstrap';
+import DaumPostcode from 'react-daum-postcode';
 
 const Signup = () => {
     const [form, setForm] = useState({
@@ -26,7 +27,8 @@ const Signup = () => {
         }
     }
     const [emailCheckMessage, setEmailCheckMessage] = useState('');
-    const [isEmailChecked, setIsEmailChecked] = useState('false');
+    const [isEmailChecked, setIsEmailChecked] = useState(false);
+    const [showPostcode, setShowPostcode] = useState(false)
     const checkEmail = () => {
         axios.get('/react/email-check', {params: {email:form.email}})
              .then(result => {
@@ -41,6 +43,25 @@ const Signup = () => {
              .catch(() => {
                 alert('이메일 확인 중 오류가 발생했습니다.');
              })
+    }
+
+    const handleComplete = (data) => {
+        setForm({...form, address: data.address});
+        setShowPostcode(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(!isEmailChecked) {
+            alert("이메일 중복 확인을 해주세요.");
+            return;
+        }
+        axios.post('/react/signup', form)
+             .then(() => {
+                alert('회원가입 성공');
+                window.location.href = '/';
+             })
+             .catch(() => alert('회원가입 실패'))
     }
     return (
         <div style={style.container}>
@@ -66,28 +87,28 @@ const Signup = () => {
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">PASSWORD</Form.Label>
                     <Col sm="10">
-                    <Form.Control type="password" name="password" required/>
+                    <Form.Control type="password" name="password" onChange={handleChange} required/>
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">NAME</Form.Label>
                     <Col sm="10">
-                    <Form.Control name="name" required/>
+                    <Form.Control name="name" onChange={handleChange} required/>
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">BIRTHDAY</Form.Label>
                     <Col sm="10">
-                    <Form.Control type="date" name="birthday" />
+                    <Form.Control type="date" name="birthday" onChange={handleChange} />
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">GENDER</Form.Label>
                     <Col sm="10">
-                    <Form.Select name="gender">
+                    <Form.Select name="gender" onChange={handleChange}>
                             <option>성별 선택</option>
                             <option value="M">남성</option>
                             <option value="F">여성</option>
@@ -98,27 +119,37 @@ const Signup = () => {
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">PHONE</Form.Label>
                     <Col sm="10">
-                    <Form.Control name="phone" />
+                    <Form.Control name="phone" onChange={handleChange} />
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">ADDRESS</Form.Label>
                     <Col sm="8">
-                    <Form.Control name="address" readOnly />
+                    <Form.Control name="address" value={form.address} readOnly />
                     </Col>
                     <Col sm="2">
-                    <Button >주소 검색</Button>
+                    <Button onClick={() => setShowPostcode(true)}>주소 검색</Button>
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} className="mb-3" >
                     <Form.Label column sm="2">DETAIL ADDRESS</Form.Label>
                     <Col sm="10">
-                    <Form.Control name="detailAddress" />
+                    <Form.Control name="detailAddress" onChange={handleChange}/>
                     </Col>
                 </Form.Group>
+                <Button type="submit">회원가입</Button>
             </Form>
+
+            <Modal show={showPostcode} onHide={() => setShowPostcode(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>주소 검색</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <DaumPostcode onComplete={handleComplete}/>
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
